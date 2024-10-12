e397d6fbd2aa1cd93b5f8aeef2cab719991627fe
observerUA <kirillkuznetsov181@gmail.com>
2024-10-12 23:26:59 +0300

    first commit

---
+++ src/main/java/com/postal/PostalServiceApplication.java
@@ -0,0 +1,12 @@
+package com.postal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostalServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/postal/PostalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
